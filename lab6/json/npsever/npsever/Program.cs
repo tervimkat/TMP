939f4b597ca5aa68b06b1e2c3a01d405023985c0
tervimkat <86067975+tervimkat@users.noreply.github.com>
2021-06-18 10:10:23 +0500

    Add files via upload

---
+++ lab6/json/npsever/npsever/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace npsever
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Person person1 = new Person("Tom", 29);
+                Person person2 = new Person("Bob", 35);
+                Person[] people = new Person[] { person1, person2 };
+                using (NamedPipeServerStream pipeServer = new NamedPipeServerStream("testpipe", PipeDirection.Out))
+                {
+                    pipeServer.WaitForConnection();
+                    using (StreamWriter sw = new StreamWriter(pipeServer)) // поток для записи в канал
+                    {
+                        foreach (var elem in people)
+                        {
+                            sw.WriteLine(JsonSerializer.Serialize(elem)); // сериализуем и отправляем
+                        }
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
