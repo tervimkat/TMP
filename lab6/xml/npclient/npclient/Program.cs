3c2d6499b59a9903340167d20598842d86f37bd6
tervimkat <86067975+tervimkat@users.noreply.github.com>
2021-06-18 10:05:36 +0500

    Add files via upload

---
+++ lab6/xml/npclient/npclient/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Xml.Serialization;
+using System.Runtime.Serialization;
+using System.Reflection;
+
+namespace npclient
+{
+    [Serializable]
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Person() { }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (NamedPipeClientStream pipeClient = new NamedPipeClientStream(".", "testpipe", PipeDirection.In))
+                {
+
+                    pipeClient.Connect();
+                    XmlSerializer formatter = new XmlSerializer(typeof(Person[]));  // класс xml десериализации
+                    Person[] deserilizePeople = (Person[])formatter.Deserialize(pipeClient);  // десериализуем данные из канала
+                    foreach (Person p in deserilizePeople)
+                    {
+                        Console.WriteLine($"Имя: {p.Name} --- Возраст: {p.Age}");
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
