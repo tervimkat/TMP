a558937106fd38a7d88bfa5b4e8abad075ef9298
tervimkat <86067975+tervimkat@users.noreply.github.com>
2021-06-18 10:11:51 +0500

    Add files via upload

---
+++ lab7/bin/npsever/npsever/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Permissions;
+
+namespace npsever
+{
+    [Serializable]
+    class Person : ISerializable 
+    {
+        [NonSerialized()]
+        public int Id;
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        [OptionalField]
+        public string t;
+        public Person(int id,string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            t = "23";
+        }
+        public Person() { }
+        protected Person(SerializationInfo info, StreamingContext context)
+        {
+            Name = info.GetString("Name");
+            t = info.GetString("t");
+            Age = info.GetInt32("Age");
+        }
+        [SecurityPermissionAttribute(SecurityAction.Demand, SerializationFormatter = true)]
+        public virtual void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Name", Name);
+            info.AddValue("Age", Age);
+            info.AddValue("t", t);
+        }
+        [OnDeserializing]
+        private void SetValuesOnDeserializing(StreamingContext context)
+        {
+            Id = -1;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Person person1 = new Person(1,"Tom", 29);
+                Person person2 = new Person(2,"Bob", 35);
+                Person[] people = new Person[] { person1, person2 };
+                using (NamedPipeServerStream pipeServer = new NamedPipeServerStream("testpipe", PipeDirection.Out))
+                {
+                    pipeServer.WaitForConnection();
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    formatter.Serialize(pipeServer, people);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
