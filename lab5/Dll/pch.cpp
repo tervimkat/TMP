1a44d4fd0a90526337de7362645373105f7ca3b5
tervimkat <86067975+tervimkat@users.noreply.github.com>
2021-06-18 09:52:46 +0500

    Add files via upload

---
+++ lab5/Dll/pch.cpp
@@ -0,0 +1,10 @@
+﻿ #include "pch.h"
+
+// метод передачи структуры
+__declspec(dllexport)  MyStruct getStruct()
+{
+	MyStruct st;
+	st.time = time(NULL);
+	st.value = sin(st.time);
+	return st;
+}
---
+++ lab5/Dll/pch.h
@@ -0,0 +1,21 @@
+﻿
+#ifndef PCH_H
+#define PCH_H
+#ifdef LIBRARY_EXPORTS
+#    define LIBRARY_API __declspec(dllexport)
+#else
+#    define LIBRARY_API __declspec(dllimport)
+#endif
+#include <ctime>
+#include <cmath>
+// при импорте указываем, что будет передана структура и метод по правилам имеинования C
+extern "C" struct LIBRARY_API  MyStruct
+{
+	time_t time;
+	double value;
+};
+extern "C" LIBRARY_API  MyStruct getStruct();
+// Добавьте сюда заголовочные файлы для предварительной компиляции
+#include "framework.h"
+
+#endif //PCH_H
