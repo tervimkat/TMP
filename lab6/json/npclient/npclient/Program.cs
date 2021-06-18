176052819e30e1c135d718cfd3be68b774de72f2
tervimkat <86067975+tervimkat@users.noreply.github.com>
2021-06-18 10:08:32 +0500

    Add files via upload

---
+++ lab6/json/npclient/npclient/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Reflection;
+
+namespace npclient
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (NamedPipeClientStream pipeClient = new NamedPipeClientStream(".", "testpipe", PipeDirection.In))
+                {
+                    pipeClient.Connect();
+                    using (StreamReader sr = new StreamReader(pipeClient)) // поток для принятия данных
+                    {
+                        string temp;
+                        while ((temp = sr.ReadLine()) != null)
+                        {
+                            Person p = JsonSerializer.Deserialize<Person>(temp);
+                            Console.WriteLine($"Имя: {p.Name} --- Возраст: {p.Age}");
+                        }
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
